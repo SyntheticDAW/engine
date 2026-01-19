bfa3a54cad4bb0ac4503a0db767eafa07bea08f7
Winter <qaiikprogramming@gmail.com>
2026-01-19 11:10:39 -0700

    push

--- src/wasm/hann_api.ts
+++ src/wasm/hann_api.ts
@@ -13,16 +13,16 @@ export async function loadHann() {
 }
 
 export function makeBlock(n: number) {
-    const ptr = (hannInstance!.instance.exports as any).alloc_f64(n);
-    const view = new Float64Array((hannInstance?.instance.exports.memory as any).buffer, ptr, n);
+    const ptr = (hannInstance!.instance.exports as any).alloc_f32(n);
+    const view = new Float32Array((hannInstance?.instance.exports.memory as any).buffer, ptr, n);
     (view as any).ptr = ptr;
     (view as any).free = function () {
-        (hannInstance!.instance.exports as any).free_f64(ptr, n);
+        (hannInstance!.instance.exports as any).free_f32(ptr, n);
     }
     return view;
 }
 
-export function hann(block: Float64Array & { free(): void; ptr: number }): Float64Array {
+export function hann(block: Float32Array & { free(): void; ptr: number }): Float32Array {
     (hannInstance!.instance.exports as any).hann(block.ptr, block.length);
     return block;
 }
